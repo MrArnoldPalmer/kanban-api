cfccbfa51545077c000566d4429d8d0ac7d44f44
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-02-07 12:38:29 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# kanban-api
+
+## Installation
+
+Download from https://github.com/mrarnoldpalmer/kanban-api
+
+## Usage
+
+    $ java -jar kanban-api-0.1.0-standalone.jar [args]
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
+
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject kanban-api "0.1.0-SNAPSHOT"
+  :description "GraphQl api for https://github.com/mrarnoldpalmer/kanban"
+  :url "https://github.com/mrarnoldpalmer/kanban-api"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main ^:skip-aot kanban-api.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
