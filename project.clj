2170957af600242cf593f5bc51e6187f705e4bae
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-02-10 20:41:02 -0800

    Added reload back to wercker dev pipeline. Sample mongodb operation in main

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/mrarnoldpalmer/kanban-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.novemberain/monger "3.0.2"]]
   :main ^:skip-aot kanban-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/kanban_api/core.clj
+++ src/kanban_api/core.clj
@@ -1,7 +1,18 @@
 (ns kanban-api.core
-  (:gen-class))
+  (:require [monger.core :as mongo]
+            [monger.collection :as mc]))
+
+(def conn (mongo/connect {:host (System/getenv "MONGO_PORT_27017_TCP_ADDR")}))
+(def db (mongo/get-db conn "kanban"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (mc/insert db "documents" { :_id "test" :first_name "John" :last_name "Lennon" })
+  (println (mc/find-maps db "documents"))
   (println "Hello, World!"))
+
+
+
+
+
