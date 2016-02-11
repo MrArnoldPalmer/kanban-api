11c2183601aa22f369f7c315d6b0bd2bc2fa2e1e
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-02-11 14:33:49 -0800

    Base route handler working, wercker dev command runs lein ring server

--- project.clj
+++ project.clj
@@ -4,7 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.novemberain/monger "3.0.2"]]
+                 [com.novemberain/monger "3.0.2"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [compojure "1.4.0"]]
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler kanban-api.core/app}
   :main ^:skip-aot kanban-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/kanban_api/core.clj
+++ src/kanban_api/core.clj
@@ -1,17 +1,24 @@
 (ns kanban-api.core
   (:require [monger.core :as mongo]
-            [monger.collection :as mc]))
+            [monger.collection :as mc]
+            [compojure.core :refer :all]
+            [compojure.route :as route]))
 
 (def conn (mongo/connect {:host (System/getenv "MONGO_PORT_27017_TCP_ADDR")}))
 (def db (mongo/get-db conn "kanban"))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (mc/insert db "documents" { :_id "test" :first_name "John" :last_name "Lennon" })
-  (println (mc/find-maps db "documents"))
-  (println "Hello, World!"))
+(defroutes app
+  (GET "/" [] "<h1>Hello World</h1>"))
 
+
+
+;;(defn -main
+;;  "I don't do a whole lot ... yet."
+;;  [& args]
+;;  (mc/insert db "documents" { :_id "test" :first_name "John" :last_name "Lennon" })
+;;  (println (mc/find-maps db "documents"))
+;;  (println "Hello, World!"))
+;;
 
 
 
