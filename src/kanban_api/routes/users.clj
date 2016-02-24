38fd4700c81d67d05639a287a2455c139d9aea02
Mitchell Valine <mitchell.valine@mirumagency.com>
2016-02-23 18:25:46 -0800

    added get by id for users

--- src/kanban_api/routes/users.clj
+++ src/kanban_api/routes/users.clj
@@ -1,8 +1,10 @@
 (ns kanban-api.routes.users
   (:require [compojure.core :refer [GET POST defroutes]]
             [kanban-api.db.core :refer [db]]
-            [monger.collection :as mc]))
+            [monger.collection :as mc])
+  (:import org.bson.types.ObjectId))
 
 (defroutes routes
   (GET "/users" [] (mc/find-maps db "users"))
+  (GET "/users/:id" [id] (mc/find-one-as-map db "users" (ObjectId. id)))
   (POST "/users" {body :body} (println "POST to users")))
